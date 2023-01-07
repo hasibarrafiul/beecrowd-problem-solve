65dfd60dff3f8a782262ceb1dfa950019f66cbe6
Hasib Ar Rafiul Fahim <85202959+hasibarrafiul@users.noreply.github.com>
2023-01-07 21:47:12 +0600

    Create 1012.cpp

---
+++ 1012.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void){
+    double A,B,C,TRIANGULO,CIRCULO,TRAPEZIO,QUADRADO,RETANGULO,pi=3.14159;
+    scanf("%lf %lf %lf",&A,&B,&C);
+    TRIANGULO=(A*C)/2;
+    CIRCULO=C*C*pi;
+    TRAPEZIO=((A + B) / 2) * C;
+    QUADRADO=B*B;
+    RETANGULO=A*B;
+    printf("TRIANGULO: %.3lf\n",TRIANGULO);
+    printf("CIRCULO: %.3lf\n",CIRCULO);
+    printf("TRAPEZIO: %.3lf\n",TRAPEZIO);
+    printf("QUADRADO: %.3lf\n",QUADRADO);
+    printf("RETANGULO: %.3lf\n",RETANGULO);
+    return 0;
+}
