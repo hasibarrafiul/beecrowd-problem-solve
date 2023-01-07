ee37977907e93d70ff5174b7365083abfc20c1ec
Hasib Ar Rafiul Fahim <85202959+hasibarrafiul@users.noreply.github.com>
2023-01-07 21:45:07 +0600

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    
+    int A,B;
+    float C,S;
+    scanf("%d %d",&A,&B);
+    scanf("%f",&C);
+    S=B*C;
+    printf("NUMBER = %d\n",A);
+    printf("SALARY = U$ %.2f\n",S);
+    return 0;
+}
