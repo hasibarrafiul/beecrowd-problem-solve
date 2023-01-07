b1a59b7bc41a62f5fc18cd748bdd578808adb9aa
Hasib Ar Rafiul Fahim <85202959+hasibarrafiul@users.noreply.github.com>
2023-01-07 21:52:42 +0600

    Create 1020.cpp

---
+++ 1020.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() { 
+    int n;
+     scanf("%d", &n); 
+     printf("%d ano(s)\n", n/365);
+     n %= 365;
+     printf("%d mes(es)\n", n/30);
+     n %= 30;
+     printf("%d dia(s)\n", n/1);
+     return 0;
+     }
